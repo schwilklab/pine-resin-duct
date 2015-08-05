d1a9912e6f4c1a076eb6a38ea2e3a96c4ef89fe0
Erik Lindberg <erik.lindberg@ttu.edu>
2015-08-04 20:28:51 -0500

    Add samples from Davis mountain trip in July

---
+++ scripts/species_count.R
@@ -0,0 +1,10 @@
+species.count <-read.csv("C://Users/Erik/Documents/GitHub/bark-beetle/data/masters_trees.csv")
+species.count$spcode
+sum(species.count$spcode=="PIPO" & species.count$mtn=="GM" & species.count$condition=="alive")
+sum(species.count$spcode=="PIPO" & species.count$mtn=="DM" & species.count$condition=="alive")
+sum(species.count$spcode=="PIAR" & species.count$mtn=="CM" & species.count$condition=="alive")
+sum(species.count$spcode=="PICE" & species.count$mtn=="GM" & species.count$condition=="alive")
+sum(species.count$spcode=="PICE" & species.count$mtn=="DM" & species.count$condition=="alive")
+sum(species.count$spcode=="PIED" & species.count$mtn=="GM" & species.count$condition=="alive")
+sum(species.count$spcode=="PIST3"& species.count$mtn=="GM" & species.count$condition=="alive")
+sum(species.count$spcode=="PIST3"& species.count$mtn=="DM" & species.count$condition=="alive")
