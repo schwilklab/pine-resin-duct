11ea6668e5e7344b8d4b25832a58fd9c237f5638
Dylan W. Schwilk <dylan@schwilk.org>
2015-09-04 13:55:57 -0500

    Fix #5
    Always use relative paths

--- scripts/species_count.R
+++ scripts/species_count.R
@@ -1,30 +1,37 @@
-species.count <-read.csv("C://Users/Erik/Documents/GitHub/bark-beetle/data/masters_trees.csv")
-species.count$spcode
+## species_count.R
+
+## replication counts and data integrity checks
+
+
+# read tree data
+trees <- read.csv("../data/masters_trees.csv")
+
+trees$spcode
 # Code for sample count of cores taken per species in each mountain range
 # Guadalupe Mountains
-sum(species.count$spcode=="PIPO" & species.count$mtn=="GM" & species.count$condition=="alive")
-sum(species.count$spcode=="PIED" & species.count$mtn=="GM" & species.count$condition=="alive")
-sum(species.count$spcode=="PIST3" & species.count$mtn=="GM" & species.count$condition=="alive")
+sum(trees$spcode=="PIPO" & trees$mtn=="GM" & trees$condition=="alive")
+sum(trees$spcode=="PIED" & trees$mtn=="GM" & trees$condition=="alive")
+sum(trees$spcode=="PIST3" & trees$mtn=="GM" & trees$condition=="alive")
 # Davis Mountains
-sum(species.count$spcode=="PIPO" & species.count$mtn=="DM" & species.count$condition=="alive" &
-      species.count$core.taken=="Y")
-sum(species.count$spcode=="PICE" & species.count$mtn=="DM" & species.count$condition=="alive" &
-      species.count$core.taken=="Y")
-sum(species.count$spcode=="PIST3"& species.count$mtn=="DM" & species.count$condition=="alive" &
-      species.count$core.taken=="Y")
+sum(trees$spcode=="PIPO" & trees$mtn=="DM" & trees$condition=="alive" &
+      trees$core.taken=="Y")
+sum(trees$spcode=="PICE" & trees$mtn=="DM" & trees$condition=="alive" &
+      trees$core.taken=="Y")
+sum(trees$spcode=="PIST3"& trees$mtn=="DM" & trees$condition=="alive" &
+      trees$core.taken=="Y")
 # Chisos Mountains
-sum(species.count$spcode=="PIAR" & species.count$mtn=="CM" & species.count$condition=="alive")
-sum(species.count$spcode=="PICE" & species.count$mtn=="CM" & species.count$condition=="alive")
+sum(trees$spcode=="PIAR" & trees$mtn=="CM" & trees$condition=="alive")
+sum(trees$spcode=="PICE" & trees$mtn=="CM" & trees$condition=="alive")
 
 # Code for number of needles samples per each species in each mountain range
 # Guadalupe Mountains
-sum(species.count$spcode=="PICE"& species.count$mtn=="GM" & species.count$needles.collected=="Y")
-sum(species.count$spcode=="PIPO"& species.count$mtn=="GM" & species.count$needles.collected=="Y")
-sum(species.count$spcode=="PIST3"& species.count$mtn=="GM" & species.count$needles.collected=="Y")
+sum(trees$spcode=="PICE"& trees$mtn=="GM" & trees$needles.collected=="Y")
+sum(trees$spcode=="PIPO"& trees$mtn=="GM" & trees$needles.collected=="Y")
+sum(trees$spcode=="PIST3"& trees$mtn=="GM" & trees$needles.collected=="Y")
 # Davis Mountains
-sum(species.count$spcode=="PIST3"& species.count$mtn=="DM" & species.count$needles.collected=="Y")
-sum(species.count$spcode=="PIPO"& species.count$mtn=="DM" & species.count$needles.collected=="Y")
-sum(species.count$spcode=="PICE"& species.count$mtn=="DM" & species.count$needles.collected=="Y")
+sum(trees$spcode=="PIST3"& trees$mtn=="DM" & trees$needles.collected=="Y")
+sum(trees$spcode=="PIPO"& trees$mtn=="DM" & trees$needles.collected=="Y")
+sum(trees$spcode=="PICE"& trees$mtn=="DM" & trees$needles.collected=="Y")
 # Chisos Mountains
-sum(species.count$spcode=="PIPO"& species.count$mtn=="CM" & species.count$needles.collected=="Y")
-sum(species.count$spcode=="PIAR"& species.count$mtn=="CM" & species.count$needles.collected=="Y")
+sum(trees$spcode=="PIPO"& trees$mtn=="CM" & trees$needles.collected=="Y")
+sum(trees$spcode=="PIAR"& trees$mtn=="CM" & trees$needles.collected=="Y")
