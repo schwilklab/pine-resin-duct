c5caa91d3f451176f3a0b82302283e65d71417e3
Erik Lindberg <erik.lindberg@ttu.edu>
2015-10-02 00:45:29 -0500

    Added whether pith was present or not in the script

--- scripts/species_count.R
+++ scripts/species_count.R
@@ -8,7 +8,7 @@ library(plyr)
 trees <- read.csv("../data/masters_trees.csv")
 
 # cores
-ddply(subset(trees, condition=="alive" & core.taken=="Y"), .(mtn, spcode), summarize, N = length(tag))
+ddply(subset(trees, condition=="alive" & core.taken=="Y" & pith=="Y"), .(mtn, spcode), summarize, N = length(tag))
 
 # needles
 ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode), summarize, N = length(tag))
