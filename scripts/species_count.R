16d504cc5b9660649ae0175c9400f8ac3b51c8cc
Lizz Waring <elizabeth.waring@ttu.edu>
2015-10-06 09:46:24 -0500

    Added code on analysis we discussed

--- scripts/species_count.R
+++ scripts/species_count.R
@@ -8,7 +8,19 @@ library(plyr)
 trees <- read.csv("../data/masters_trees.csv")
 
 # cores
-ddply(subset(trees, condition=="alive" & core.taken=="Y"), .(mtn, spcode), summarize, N = length(tag))
+ddply(subset(trees, condition=="alive" & core.taken=="Y"), .(mtn, spcode), 
+      summarize,
+      N = length(tag))
+# EFW: clean up look of code. You need a wrapper or stop at ~80 characters to 
+# improve readability
 
 # needles
-ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode), summarize, N = length(tag))
+ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode), 
+      summarize,
+      N = length(tag))
+
+# EFW: Testing how BAF is effected by mountain range and species
+
+baf.lm <- lm(BAF ~ mtn + spcode + mtn:spcode, data=trees)
+summary(baf.lm)
+anova(baf.lm)
