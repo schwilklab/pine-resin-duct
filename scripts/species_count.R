ed65396c65feb1134050e908b779099c10647c24
Erik Lindberg <erik.lindberg@ttu.edu>
2015-10-07 14:42:49 -0500

    Cleaned up code from changes Lizz made

--- scripts/species_count.R
+++ scripts/species_count.R
@@ -2,7 +2,13 @@
 ## ---------------
 ## replication counts and data integrity checks
 
+# Clears the workspace
+rm(list=ls())
+
+# Load programs
 library(plyr)
+library(ggplot2)
+update.packages()
 
 # read tree data
 trees <- read.csv("../data/masters_trees.csv")
@@ -15,19 +21,12 @@ ddply(subset(trees, condition=="alive" & core.taken=="Y" & pith=="Y"),
 ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode),
       summarize, N = length(tag))
 
-# EFW: Clean up code. Want to include either a wrapper or make sure your code
-# Doesn't go past 80 characters to improve readability. Here's 80 ------------!
 
-
-# EFW: Added in analysis of BAF. Simple linear regression followed by a ANOVA
-
+# Linear regression of BAF followed by ANOVA
 baf.lm <- lm(BAF~ mtn + spcode + spcode:mtn, data=trees)
 summary(baf.lm)
 anova(baf.lm)
 
 # There is a difference between species.  Looking at figure to get better idea
-
-library(ggplot2)
-
 ggplot(trees, aes(spcode, BAF, color=mtn)) +
   geom_point()
