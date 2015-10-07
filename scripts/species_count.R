022c0d7d3e354de7276a30562fa754f9be1caf3d
Lizz Waring <elizabeth.waring@ttu.edu>
2015-10-07 11:39:30 -0500

    Clean up readability

--- scripts/species_count.R
+++ scripts/species_count.R
@@ -8,7 +8,14 @@ library(plyr)
 trees <- read.csv("../data/masters_trees.csv")
 
 # cores
-ddply(subset(trees, condition=="alive" & core.taken=="Y" & pith=="Y"), .(mtn, spcode), summarize, N = length(tag))
+ddply(subset(trees, condition=="alive" & core.taken=="Y" & pith=="Y"), 
+      .(mtn, spcode), summarize, N = length(tag))
 
 # needles
-ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode), summarize, N = length(tag))
+ddply(subset(trees, needles.collected=="TRUE"), .(mtn, spcode),
+      summarize, N = length(tag))
+
+# EFW: Clean up code. Want to include either a wrapper or make sure your code
+# Doesn't go past 80 characters to improve readability. Here's 80 ------------!
+
+
