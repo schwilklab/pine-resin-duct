bdac035b23c21b5cad9905f6d1502828d3629ec8
Dylan W. Schwilk <dylan@schwilk.org>
2015-10-09 11:19:27 -0500

    Leave original date column in precip data
    Make new year column, convert to integer, run summary

--- scripts/precip_data.R
+++ scripts/precip_data.R
@@ -5,16 +5,21 @@
 # Load programs
 library(plyr)
 library(stringr)
+library(ggplot2)
 
 # Load .csv file from dropbox
 monthly_precip_data <- read.csv("https://www.dropbox.com/s/iqi7e4n8sietwtu/precip_data.
                         csv?dl=1")
 # Remove numbers for months in TPCP column
 str_sub(monthly_precip_data$DATE, 5, -1) <- "";
+monthly_precip_data$year <- as.integer(str_sub(monthly_precip_data$DATE, 1, 4))
 
 # Summarize monthly precipitation averages to obtain yearly averages
-yearly_precip_data <- (ddply(subset(monthly_precip_data), 
-      .(STATION_NAME,YEAR= DATE),summarize, PRECIP = sum(TPCP)))
-
+yearly_precip_data <- (ddply(monthly_precip_data,
+                       .(STATION_NAME, year), summarize, PRECIP = sum(TPCP)))
 head(yearly_precip_data)
+
+ggplot(yearly_precip_data, aes(year, PRECIP)) +
+           geom_line() +
+           facet_grid(STATION_NAME ~ .)
 
