2b7c8cfc56b3cae6f11529cda9f245636b9528dd
Erik Lindberg <erik.lindberg@ttu.edu>
2016-02-22 23:42:03 -0600

    Modified script to account for new column
    Added the resin.duct.count column to all .csv files.  Modified code to
    run with the new information.  Also, converted the ring.dist values from
    inches to centimeters.  Lastly, added cleanup part at the end to remove
    temporary values that were still populating the global and local
    environment.

--- scripts/rings.R
+++ scripts/rings.R
@@ -1,7 +1,9 @@
 # rings.R
 
-# Read in tree ring coordinates from individual csv files, calcualte ring
-# widths and areas. Provides data frame "ring_data" to global namespace
+# Make sure working directory is set to "bark-beetle"
+
+# Read in tree ring coordinates from individual csv files, calculate ring
+# widths and areas. Provides data frame "ring_data" to global namespace.
 
 library(plyr)
 
@@ -14,18 +16,19 @@ get_widths <- function(distances) {
     c(distances[2:length(distances)], NA) - distances
 }
 
-ring_files <- list.files("../data/tree_ring_coordinates", pattern = "*.csv")
+ring_files <- list.files("./data/tree_ring_coordinates")
 ring_data = data.frame()
 for (f in ring_files) {
-    df <- read.csv(file.path("../data/tree_ring_coordinates", f))
-    names(df) <- c("ring", "x", "y")
+    df <- read.csv(file.path("./data/tree_ring_coordinates", f))
+    names(df) <- c("ring", "x", "y", "resin.duct.count")
     df$tag <- strsplit(f, "\\.")[[1]][1]
     # "ring number/id" is count from center of bole.
-    df$ring.dist <- sdist(df$x[1],df$y[1], df$x, df$y)
+    df$ring.dist <- sdist(df$x[1],df$y[1], df$x, df$y)*2.54
+    # Converting value obtained from inches to cm
     df$ring.width <- get_widths(df$ring.dist)
     df <- df[-nrow(df),]
     ring_data <- rbind.fill(ring_data, df)
 }
 
 # clean up
-rm(ring_files)
+rm(ring_files, df, f)
