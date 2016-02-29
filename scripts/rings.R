7fa36e0c379cd2662bdd4af497d0cdefbad9d277
Dylan W. Schwilk <dylan@schwilk.org>
2016-02-29 13:49:25 -0600

    Fix #14

--- scripts/rings.R
+++ scripts/rings.R
@@ -1,12 +1,16 @@
 # rings.R
 
-# Make sure working directory is set to "bark-beetle"
-
 # Read in tree ring coordinates from individual csv files, calculate ring
 # widths and areas. Provides data frame "ring_data" to global namespace.
 
-library(plyr)
-trees <- read.csv("./data/masters_trees.csv") #needed for tree age
+library(dplyr)
+library(ggplot2)
+
+trees <- read.csv("../data/masters_trees.csv", stringsAsFactors=FALSE) # needed for tree age
+## DWS: How tree age?
+
+SAMPLE_YEAR <- 2015 # year trees were cored, so last full ring should be this
+                    # minus one
 
 sdist <- function(x1,y1,x2,y2) {
     sqrt( (x1-x2)^2 + (y1-y2)^2)
@@ -17,10 +21,11 @@ get_widths <- function(distances) {
     c(distances[2:length(distances)], NA) - distances
 }
 
-ring_files <- list.files("./data/tree_ring_coordinates")
+ring_files <- list.files("../data/tree_ring_coordinates")
 ring_data = data.frame()
 for (f in ring_files) {
-    df <- read.csv(file.path("./data/tree_ring_coordinates", f))
+    df <- read.csv(file.path("../data/tree_ring_coordinates", f),
+                   stringsAsFactors=FALSE)
     names(df) <- c("ring", "x", "y", "resin.duct.count")
     for (x in length(df)) {
       df$temporary<- sort(df$ring, decreasing=TRUE)
