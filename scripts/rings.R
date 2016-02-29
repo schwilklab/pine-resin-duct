574f034336498a456c770657c0b092ed5bdd5089
Dylan W. Schwilk <dylan@schwilk.org>
2016-02-29 16:49:04 -0600

    Wrap ring coord file reading into function
    - Wrap up reading of ring coordinate csv files into a function and use
      lapply to make code easier to read and more brief
    - Document ring calendar year assignment better (fixes #15)

--- scripts/rings.R
+++ scripts/rings.R
@@ -1,49 +1,63 @@
 # rings.R
 
 # Read in tree ring coordinates from individual csv files, calculate ring
-# widths and areas. Provides data frame "ring_data" to global namespace.
+# widths and areas. Provides data frame "ring_data" to global namespace. The
+# tree ring data files include a row for each ring and a final row giving the
+# coordinates of the vascular cambium. So the final row of data is only used to
+# calculate the width associated with the outermost ring (penultimate row of
+# data).
 
 library(dplyr)
 library(ggplot2)
 
-trees <- read.csv("../data/masters_trees.csv", stringsAsFactors=FALSE) # needed for tree age
-## DWS: How tree age?
-
-SAMPLE_YEAR <- 2015 # year trees were cored, so last full ring should be this
-                    # minus one
+SAMPLE_YEAR <- 2015 # year trees were cored, so last full ring will be this one
+                    # assuming that sampling occured after enough growth to
+                    # distinguish. The code should porbably actually check the
+                    # sample date in trees.csv, but this is ok for now if all
+                    # were sampled in mid-late summer 2015.
 
 sdist <- function(x1,y1,x2,y2) {
     sqrt( (x1-x2)^2 + (y1-y2)^2)
 }
 
-
+# Obtain the distance between rings. Coordinates are associated with the inner
+# boundary of a ring, so we obtain values for rows n:(n-1).
 get_widths <- function(distances) {
     c(distances[2:length(distances)], NA) - distances
 }
 
-ring_files <- list.files("../data/tree_ring_coordinates")
-ring_data = data.frame()
-for (f in ring_files) {
-    df <- read.csv(file.path("../data/tree_ring_coordinates", f),
-                   stringsAsFactors=FALSE)
+# Reads a ring coordinate file and returns a data frame with several new
+# calculated columns: calendar year, age, ring.dist and ring.width
+read_ring_coord_file <- function(filename) {
+    df <- read.csv(filename, stringsAsFactors=FALSE)
     names(df) <- c("ring", "x", "y", "resin.duct.count")
-    df$tag <- strsplit(f, "\\.")[[1]][1]
+    df$tag <- strsplit(basename(filename), "\\.")[[1]][1]
     x1 <- df$x[1]
     y1 <- df$y[1]
-    df <- df %>% mutate(calendar.year=SAMPLE_YEAR + ring - max(ring), #or +1?
+    df <- df %>% mutate(calendar.year=SAMPLE_YEAR + ring - max(ring) + 1,
                         age=max(ring)-ring,
-                        # Convert value obtained from inches to cm
+                        # Convert coordinate pixels from inches to cm
                         ring.dist=sdist(x1, y1, x, y)*2.54,
                         ring.width=get_widths(ring.dist))
-    ring_data <- rbind(ring_data, df)
+    return(subset(df, !is.na(ring.width)) ) # throw away last row in each df
 }
 
-# clean up
-rm(ring_files, df, f, x)
-ring_data <- ring_data %>% subset(!is.na(ring.width)) %>%
+# needed for tree age:
+trees <- read.csv("../data/masters_trees.csv", stringsAsFactors=FALSE)
+## DWS: How tree age?
+
+ring_files <- list.files("../data/tree_ring_coordinates", full.names=TRUE)
+
+# create a list of coordinate dataframes (1 per tree), concatenate these all
+# into one df, then merge (innner_join) with the trees.csv data
+ring_data <- rbind_all(lapply(ring_files, read_ring_coord_file)) %>%
     inner_join(trees)
 
+# clean up unneeded variables
+rm(ring_files)
+
 # Exploring data
-ggplot(ring_data, aes(age, resin.duct.count/ring.width)) +
+ggplot(ring_data, aes(age, resin.duct.count/ring.width, color=mtn)) +
     geom_point() +
+    scale_y_log10() +
     facet_grid(spcode ~ .)
