76375f1357cc2403309eb81a3f0e8f0c4b2cd209
Erik Lindberg <erik.lindberg@ttu.edu>
2016-04-15 00:04:07 -0500

    Added graph info, and new calculations
    Updated graphs from TTABSS talk.  Also added new calculations looking at
    BAI by BAF and duct density by tree age.

--- scripts/resin_duct_calculations.R
+++ scripts/resin_duct_calculations.R
@@ -5,6 +5,7 @@
 
 source("./data-checks.R")
 source("./rings.R")
+source("./graph-themes.R")
 
 # Calculate summaries per tree
 trees.sum <- ring_data %>% group_by(tag) %>%
@@ -16,22 +17,39 @@ trees.sum <- ring_data %>% group_by(tag) %>%
               duct.den.mean = mean(duct.density, na.rm= TRUE),
               duct.den.sd = sd(duct.density, na.rm= TRUE),
               ring.width.mean = mean(ring.width),
-              ring.width.sd = sd(ring.width)
+              ring.width.sd = sd(ring.width),
+              bai.mean = mean(bai),
+              bai.sd = sd(bai)
               ) %>% inner_join(trees)
 
 # Plot mean resin duct count per year
-ggplot(trees.sum, aes(spcode, duct.count.mean, color=mtn)) +
+ggplot(trees.sum, aes(spcode, duct.count.mean, fill=mtn)) +
   geom_violin()+
-    labs(x= "Species",
-         y= "Mean resin duct count per year")
+  labs(x= "Species",
+       y= "Mean resin duct count per year")+
+  scale_x_discrete(labels= species_names)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  scale_fill_manual(name= "Mountain range",
+                    labels = mountain_names,
+                    values= mycolours)
 
 # No evidence of strong age effect
 ggplot(ring_data, aes(age, duct.density, color=mtn)) +
-    facet_grid(spcode ~ .) + geom_point() + geom_jitter()
-
+    facet_grid(spcode ~ ., labeller = as_labeller(species_names)) +
+    geom_point() +
+    geom_jitter() +
+    labs(x = "Age (years)",
+         y = "Resin duct density (resin ducts/area)")
 
 ggplot(trees.sum, aes(spcode, duct.den.mean, color=mtn)) +
     geom_violin()
+
+# Plot bai for species
+ggplot(trees.sum, aes(spcode, bai.mean, color=mtn)) +
+  geom_violin()+
+  labs(x= "Species",
+       y= "Mean basal area increment")
+  
 
 #  geom_pointrange(aes(ymin=avg.resin.year-sem, ymax=avg.resin.year+sem), size = 1)+
 #  labs(x= "Species", y= "Mean Resin Duct Count Per Year +/- SEM")
@@ -45,10 +63,14 @@ ggplot(avg.age, aes(spcode, avg.age, color=mtn)) +
 
 # does BAF predict counts?
 ggplot(trees.sum, aes(BAF, duct.count.mean, color=mtn)) +
-    facet_grid(spcode ~ .) +
-    geom_point() +
-    labs(x= "BAF",
-         y= "Mean resin duct count per year")
+  facet_grid(spcode ~ .,labeller = as_labeller(species_names_facet)) +
+  theme(strip.text.y = element_text(size=5)) +
+  geom_point() +
+  labs(x= "Basal area factor",
+       y= "Mean resin duct count per year")+
+  scale_color_manual(name= "Mountain range",
+                     labels = mountain_names,
+                     values= mycolours)
 # looks like weak signal.  Test with linear model:
 library(nlme)
 baf.mod <- lme(duct.count.mean ~ BAF, random=~1|mtn/spcode, data=trees.sum)
@@ -68,8 +90,28 @@ summary(baf.mod3)
 anova(baf.mod3)
 # yes:
 ggplot(trees.sum, aes(BAF, ring.width.mean, color=mtn)) +
-    facet_grid(spcode ~ .) +
+    facet_grid(spcode ~ .,labeller = as_labeller(species_names_facet)) +
+    theme(strip.text.y = element_text(size=5)) +
     geom_point() +
-    labs(x= "BAF",
-         y= "Mean ring width")
+    labs(x= "Basal area factor",
+         y= "Mean ring width")+
+    scale_color_manual(name= "Mountain range",
+                    labels = mountain_names,
+                    values= mycolours)
 # ok so that is reassuring
+
+# Try for bai.mean
+baf.mod4 <- lme(bai.mean ~ BAF, random=~1|mtn/spcode, data=trees.sum)
+summary(baf.mod4)
+anova(baf.mod4)
+# P = .003
+
+# Resin duct density by tree age
+age.mod1 <- lme(duct.den.mean ~ max.age, random=~1|mtn/spcode, data=trees.sum)
+summary(age.mod1)
+anova(age.mod1)
+# P= .0385 
+
+# quick plot showing duct density plotted by tree age
+ggplot(trees.sum, aes(max.age, duct.den.mean)) +
+  geom_point()
