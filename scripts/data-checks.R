52baa61bccda86a3220d3da8f7c40c395ec7a2fe
Erik Lindberg <erik.lindberg@ttu.edu>
2016-04-14 23:42:31 -0500

    Updated ggplot graphs
    Updated graphs to be more detailed from TTABSS talk.

--- scripts/data-checks.R
+++ scripts/data-checks.R
@@ -2,8 +2,9 @@
 ## ---------------
 ## replication counts and data integrity checks
 
-# Clears the workspace
-rm(list=ls())
+
+# Make sure graph-themes.R is loaded. If not:
+# source("./graph-themes.R")
 
 # Load programs
 library(plyr)
@@ -26,8 +27,17 @@ summary(baf.lm)
 anova(baf.lm)
 
 # There is a difference between species.  Looking at figure to get better idea
-ggplot(trees, aes(spcode, BAF, color=mtn)) +
-  geom_violin()
+ggplot(trees, aes(spcode, BAF, fill=mtn)) +
+  geom_violin()+
+  labs(x= "Species", y= "Basal area factor")+
+  scale_x_discrete(labels= species_names)+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+  scale_fill_manual(name= "Mountain range",
+                    labels = mountain_names,
+                    values= mycolours)
+
+
+  mycolours <- c("#FF6600", "#CC0000", "#660066", "#000999", "#3366CC", "#00CCFF")
 
 # Linear regression of DBH followed by ANOVA
 dbh.lm <- lm(DBH~ mtn + spcode + spcode:mtn, data=trees)
@@ -36,4 +46,5 @@ anova(dbh.lm)
 
 #DBH plotted by species and mountain range
 ggplot(trees, aes(spcode, DBH, color=mtn)) + 
-  geom_violin()
+  geom_violin()+
+  theme_light()
