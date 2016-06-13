7b396ba131f97b468083fb667af5bfff1078b287
Erik Lindberg <erik.lindberg@ttu.edu>
2016-06-12 22:56:31 -0500

    mods.R minor change
    Forgot to add this in my previous commit.  Ring refers to relative
    placement of the rings on the tree, so 1 refers to the pith, and n
    refers to the last ring the tree made before being cored.  This
    information is available in the metadata in ring_data-metadata.csv.

--- scripts/mods.R
+++ scripts/mods.R
@@ -5,6 +5,6 @@ library(nlme)
 # read data
 mdata <- ring_data[complete.cases(ring_data), ]
                     
-full.mod <- lme(duct.density ~ age + BAF + elev + slope + spcode + spcode:age,
+full.mod <- lme(duct.density ~ ring + BAF + elev + slope + spcode + spcode:age,
                 random = ~ 1 | tag, data=mdata, method="ML")
 full.null <-  lme(duct.density ~ 1 , random = ~ 1 | tag, data=mdata, method="ML")
