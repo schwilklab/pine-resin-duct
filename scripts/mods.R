8261a3fb125581d1149cc6dab896aa80b62d3f0c
Erik Lindberg <erik.lindberg@ttu.edu>
2016-06-17 01:07:54 -0500

    Modified two scripts
    Modified mods.R to include some further variables to include in the
    mixed model.  Also, changed the variable ring to tree.age in rings.R to
    reflect the age of the tree at each ring.

--- scripts/mods.R
+++ scripts/mods.R
@@ -2,9 +2,14 @@
 library(nlme)
 
 
-# read data
+# read data, only select complete cases so no NA's exist
 mdata <- ring_data[complete.cases(ring_data), ]
+# remove the first year of growth since no resin ducts are present in pith
+mdata <- filter(mdata, !(tree.age==1))
+# remove last year of data since these are only partial growth years
+mdata <- filter(mdata, !(calendar.year==2015))
                     
-full.mod <- lme(duct.density ~ ring + BAF + elev + slope + spcode + spcode:age,
+full.mod <- lme(duct.density ~ tree.age + BAF + elev + regional_precip + radiation
+                + PMDI + slope + spcode + bai + spcode:tree.age + mtn,
                 random = ~ 1 | tag, data=mdata, method="ML")
 full.null <-  lme(duct.density ~ 1 , random = ~ 1 | tag, data=mdata, method="ML")
